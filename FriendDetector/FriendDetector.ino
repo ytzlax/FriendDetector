ef7cbf44a926520830af16c016fa24d6f6916b02
ytzlax <ytzlax@gmail.com>
2019-06-14 08:20:30 +0300

    print all

--- FriendDetector/FriendDetector.ino
+++ FriendDetector/FriendDetector.ino
@@ -1,56 +1,53 @@
 #include "./esppl_functions.h"
 
-/*
- * Define you friend's list size here
- */
-#define LIST_SIZE 2
-/*
- * This is your friend's MAC address list
- */
-uint8_t friendmac[LIST_SIZE][ESPPL_MAC_LEN] = {
-   {0x11, 0x11, 0x11, 0x11, 0x11, 0x11}
-  ,{0x22, 0x22, 0x22, 0x22, 0x22, 0x22}
-  };
-/*
- * This is your friend's name list
- * put them in the same order as the MAC addresses
- */
-String friendname[LIST_SIZE] = {
-   "Friend 1"
-  ,"Friend 2"
-  };
-
-bool maccmp(uint8_t *mac1, uint8_t *mac2) {
-  for (int i=0; i < ESPPL_MAC_LEN; i++) {
-    if (mac1[i] != mac2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-void cb(esppl_frame_info *info) {
-  for (int i=0; i<LIST_SIZE; i++) {
-    if (maccmp(info->sourceaddr, friendmac[i]) || maccmp(info->receiveraddr, friendmac[i])) {
-      Serial.printf("\n%s is here! :)", friendname[i].c_str());
-    }
-  }
-}
-
 void setup() {
   delay(500);
   Serial.begin(115200);
   esppl_init(cb);
 }
 
-void loop() {
+String mac2String(const unsigned char* mac) {
+  char buf[20];
+  snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return String(buf);
+}
+
+String converter(uint8_t *str) {
+  return String((char *)str);
+}
+
+void cb(esppl_frame_info *info) {
+
+
+  Serial.print("sourceaddr:" );
+  Serial.println( mac2String(info->sourceaddr));
+  Serial.print("receiveraddr:" );
+  Serial.println( mac2String(info->receiveraddr));
+  Serial.print("destinationaddr:" );
+  Serial.println( mac2String(info->destinationaddr));
+  Serial.print("transmitteraddr:" );
+  Serial.println( mac2String(info->transmitteraddr));
+  Serial.print("rssi:" );
+  Serial.println( (info->rssi));
+  Serial.print("ssid:" );
+  Serial.println( converter(info->ssid));
+  Serial.print("bssid:" );
+  Serial.println( converter(info->bssid));
+  Serial.println();
+  Serial.println();
+
+
+}
+
+
+void loop() { // I didn't write this part but it sure looks fancy.
   esppl_sniffing_start();
   while (true) {
     for (int i = ESPPL_CHANNEL_MIN; i <= ESPPL_CHANNEL_MAX; i++ ) {
       esppl_set_channel(i);
       while (esppl_process_frames()) {
-        //
       }
     }
-  }  
+  }
 }
